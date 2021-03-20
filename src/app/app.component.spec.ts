aed2c8e7183ea5e437d83c295bffc21ef81462ca
2PaeDev <2paedev@gmail.com>
2021-03-20 11:52:45 +0100

    feat: add stylelint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,19 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'mi-huerto'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('mi-huerto');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'mi-huerto app is running!'
-    );
-  });
 });
